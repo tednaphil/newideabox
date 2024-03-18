77856a5559b7c08301a6945263816f884e094794
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-03-18 14:53:21 -0500

    Add test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tayla's new <code>react.js</code> ideaBox App.
         </p>
         <a
           className="App-link"
