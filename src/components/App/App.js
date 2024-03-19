ead9d2bede01d7712728c9062c64b15fe43218e2
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-03-18 20:06:04 -0500

    Import and define useState hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,21 @@ import Form from '../Form/Form';
 import Ideas from '../Ideas/Ideas';
 
 function App() {
-  return (
-    <h1>IdeaBox</h1>
-  )
+  const defaultIdeas = [
+        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' }
+  ];
+  const [ideas, setIdeas] = useState(defaultIdeas);
 
+  return (
+    <>
+      <main className='App'>
+        <h1>IdeaBox</h1>
+        <p>Heyyy</p>
+      </main>
+    </>
+  );
 }
 
 export default App;
